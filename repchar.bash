ba0927264b86c535e2ef109bcce4a20941827113
Krayon <krayon.git@qdnx.org>
2017-06-13 18:08:06 +1000

    repchar can now be ran as a script OR sourced

--- repchar.bash
+++ repchar.bash
@@ -1,42 +1,56 @@
 #!/bin/bash
 
-PROG="${0##*/}"
+# We are being run as a script, so set it's name
+[ "${0}" == "${BASH_SOURCE}" ] && _repcharbinname="${0##*/}"
 
-[ "${1}" == "--help" ] || [ "${1}" == "-h" ] && {
+# repchar -h|--help
+# repchar <char> <count>
+function repchar() {
+
+    [ "${1}" == "--help" ] || [ "${1}" == "-h" ] && {
 
 cat <<EOF
 Repeats a given character (or string) a specified number of times.
 
-Usage: ${PROG} -h|--help
-       ${PROG} <char> <count>
+Can be run directly or sourced ( . "${_repcharbinname:-repchar}" ).
+
+Usage: ${_repcharbinname:-repchar} -h|--help
+       ${_repcharbinname:-repchar} <char> <count>
 
 -h|--help           - Displays this help
 <char>              - Character (or string) to repeat.
 <count>             - Number of times to repeat <char>.
 
-Example: = 10
+Example: ${_repcharbinname:-repchar} = 10
 =========
 EOF
 
-    exit 0
-}
+        return 0
+    }
 
-# If first param is NULL, obviously the output will be nothing
-[ -z "${1}" ] && exit 0
+    # Expecting 2 params
+    [ ${#} -ne 2 ] && {
+        echo >&2 "ERROR: Invalid number of parameters"
+        return 1
+    }
 
-# Expecting 2 params
-[ ${#} -ne 2 ] && {
-    echo >&2 "ERROR: Invalid number of parameters"
-    exit 1
-}
+    # If first param is NULL, obviously the output will be nothing
+    [ -z "${1}" ] && return 0
 
-# Second param must be a number
-[ "${2}" -eq "${2}" ] &>/dev/null || {
-    echo >&2 "ERROR: Invalid <num>: ${2}"
-    exit 1
-}
+    # Second param must be a number
+    [ "${2}" -eq "${2}" ] &>/dev/null || {
+        echo >&2 "ERROR: Invalid <num>: ${2}"
+        return 1
+    }
 
-printf "${1}%.0s" $(eval echo "{1..${2}}")
-echo
+    printf "${1}%.0s" $(eval echo "{1..${2}}")
+    echo
+} # repchar
+
+# We are being sourced
+[ "${0}" != "${BASH_SOURCE}" ] && return 0
+
+# We are being run as a script
+repchar "${@}"; exit $?
 
 # vim:ts=4:sw=4:et:ai:si
